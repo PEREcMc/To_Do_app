227ba9dea68989320151e295609587dddba0b9d1
PEREcMC <perecvlasenko@gmail.com>
2022-08-02 15:37:24 +0300

    creat project - ToDo

---
+++ js/main.js
@@ -0,0 +1,138 @@
+const form = document.getElementById('form');
+const taskInput = document.getElementById('taskInput');
+const tasksList = document.getElementById('tasksList');
+const emptyList = document.getElementById('emptyList');
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    tasks.forEach(task => renderTask(task));
+};
+
+checkEmptyList();
+
+// добавление задач
+form.addEventListener('submit', addTask);
+
+function addTask(e) {
+    e.preventDefault(); // отменяем отправку формы - перезагрузку страницы
+
+    const taskText = taskInput.value;
+
+    // описываем задачу в виде объекта
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    };
+
+    // добавдяем задачу в массив с задачами
+    tasks.push(newTask);
+
+    saveToLocalStorage();
+
+    renderTask(newTask);
+
+    // очищаем поле для ввода и возвращаем фокус на него
+    taskInput.value = '';
+    taskInput.focus();
+
+    checkEmptyList();
+
+    // // удаляем emptyList при добавлении новой задачи
+    // if (tasksList.children.length > 1) {
+    //     emptyList.classList.add('none');
+    // }
+};
+
+// удаление задач
+tasksList.addEventListener('click', deleteTask);
+
+function deleteTask(e) {
+    if (e.target.dataset.action !== 'delete') return;
+
+    const parentNode = e.target.closest('.list-group-item');
+
+    // определяем id задачи
+    const idTask = Number(parentNode.id);
+
+    // находим индекс задачи в массиве
+    // const indexTask = tasks.findIndex(task => task.id === idTask);
+
+    // удаляем задачу из массива с задачами
+    // tasks.splice(index, 1)
+
+    // удаляем задачу через фильтрацию массива
+    tasks = tasks.filter(task => task.id !== idTask )
+
+    saveToLocalStorage();
+
+    parentNode.remove();
+
+    checkEmptyList();
+    // // добавляем emptyList при удалении последней задачи
+    // if (tasksList.children.length === 1) {
+    //     emptyList.classList.remove('none');
+    // }
+};
+
+// отмечаем задачу завершённой
+tasksList.addEventListener('click', doneTask);
+
+function doneTask(e) {
+    if (e.target.dataset.action !== 'done') return;
+
+        const parentNode = e.target.closest('.list-group-item');
+
+        // определяем id задачи
+        const idTask = Number(parentNode.id);
+        // изменяем статус задачи
+        task = tasks.find(task => task.id === idTask);
+        task.done = !task.done;
+
+        saveToLocalStorage();
+
+        const taskDone = parentNode.querySelector('.task-title');
+        taskDone.classList.toggle('task-title--done');
+};
+
+function checkEmptyList() {
+    if (tasks.length === 0) {
+        const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+                                    <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+                                    <div class="empty-list__title">Список дел пуст</div>
+                                 </li>`;
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+    } else {
+        const emptyListElement = document.getElementById('emptyList');
+        emptyListElement ? emptyListElement.remove() : null;
+    }
+};
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+            // формируем CSS класс
+            const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+            // формируем разметку для новой задачи
+            const taskHTML = `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+                                    <span class="${cssClass}">${task.text}</span>
+                                    <div class="task-item__buttons">
+                                        <button type="button" data-action="done" class="btn-action">
+                                            <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                                        </button>
+                                        <button type="button" data-action="delete" class="btn-action">
+                                            <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                                        </button>
+                                    </div>
+                            </li>`;
+        
+            // добавляем новую задачу на страницу
+            tasksList.insertAdjacentHTML('beforeend', taskHTML);
+};
---
+++ js/video_bq.js
@@ -0,0 +1,8 @@
+// $('body').vide('./../video/bq_video');
+
+$(window).on('load', function () {
+    
+    $('body').vide('./../video/bq_video', {  
+       bgColor: '#F8F9F9'
+    })
+});
